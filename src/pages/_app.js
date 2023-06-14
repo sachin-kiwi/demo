e7fcb50d424cb11b4191ee609e6fdd065e3a97c8
sachin-kiwi <sachin.bisht@kiwitech.com>
2023-06-15 00:20:18 +0530

    basic app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import { ToastContainer } from "react-toastify";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />;
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
 }
